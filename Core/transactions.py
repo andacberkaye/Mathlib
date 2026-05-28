229e5b4d177261897b031e70ba2ef085c721d3ed
andacberkaye <andacberkayekren@gmail.com>
2026-05-29 01:02:45 +0300

    The is_prime function has been written

--- Core/transactions.py
+++ Core/transactions.py
@@ -240,3 +240,25 @@ def parity(x):
         return "Even"
     else:
         return "Odd"
+    
+def is_prime(x):
+    """
+        Determines whether the entered number is prime
+
+        Parameters:
+            x (int | float):
+                The number to be checked for primality
+
+        Return value:
+            int | float:
+                The result of the primality check
+    """
+
+    if x <= 1:
+        return False
+
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return False
+
+    return True
